aa5106b3d3fae94e25525ebac525861bca43739d
github username <github email address>
2019-11-14 23:40:53 -0500

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
 # Create your costume_stores migration here
+class CustomeStore < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :number_of_costumes #for inventory
+      t.integer :number_of_employees
+      t.boolean :open #true of false if the store is still in business
+      t.time :opening_time
+      t.time :closing_time
+  end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class HauntedHouse < ActiveRecord::Migration[5.2]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      # t.integer :family_friendly #"Boolean" true or false
+      t.boolean :family_friendly, :default => false #"Boolean" true or false
+      t.date :opening_date
+      t.date :closing_date
+      t.blob :long_description
+  end
+end
